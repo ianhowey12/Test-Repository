37fc6895943952c1c263d33a562679d192c8ae82
Ian Howey <ianhowey@cs.utexas.edu>
2026-03-04 23:50:42 -0600

    Done

--- test.c
+++ test.c
@@ -2,9 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void side(int c){
+    printf("%d\n", c);
+}
+
 int main(){
 
-  int* a = 0;
-  printf("%d\n", *a);
-  return 0;
+    int c = 2;
+    side(c);
+
+    int* a = 0;
+    printf("%d\n", a);
+    return 0;
 }
